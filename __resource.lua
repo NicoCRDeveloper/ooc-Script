493167cf4966fba0ed6d3a32b2b034851440557d
NicoCRDeveloper <161981641+NicoCRDeveloper@users.noreply.github.com>
2024-03-02 23:44:55 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,8 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+author 'NicoCR - NCR Scripts'
+name 'ooc'
+version '1.1'
+description 'Enables use of /ooc for out of character chat.'
+server_script 'server.lua'
